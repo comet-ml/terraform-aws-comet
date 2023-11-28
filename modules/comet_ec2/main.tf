dbc95b7e2a7a705446bf78896a1e19eb2a5720dd
Martin Burke <66889764+burmek@users.noreply.github.com>
2023-11-28 16:41:00 -0500

    Enable specifying the AMI ID for EC2 instance

--- modules/comet_ec2/main.tf
+++ modules/comet_ec2/main.tf
@@ -124,14 +124,15 @@ data "aws_ami" "ubuntu22" {
 }
 
 resource "aws_instance" "comet_ec2" {
-  ami = var.comet_ec2_ami_type == "al2" ? data.aws_ami.al2.id : (
-    var.comet_ec2_ami_type == "rhel7" ? data.aws_ami.rhel7.id : (
-      var.comet_ec2_ami_type == "rhel8" ? data.aws_ami.rhel8.id : (
-        var.comet_ec2_ami_type == "rhel9" ? data.aws_ami.rhel9.id : (
-          var.comet_ec2_ami_type == "ubuntu18" ? data.aws_ami.ubuntu18.id : (
-            var.comet_ec2_ami_type == "ubuntu20" ? data.aws_ami.ubuntu20.id : (
-              var.comet_ec2_ami_type == "ubuntu22" ? data.aws_ami.ubuntu22.id : (
-  null)))))))
+  ami = var.comet_ec2_ami_id != "" ? var.comet_ec2_ami_id : (
+    var.comet_ec2_ami_type == "al2" ? data.aws_ami.al2.id : (
+      var.comet_ec2_ami_type == "rhel7" ? data.aws_ami.rhel7.id : (
+        var.comet_ec2_ami_type == "rhel8" ? data.aws_ami.rhel8.id : (
+          var.comet_ec2_ami_type == "rhel9" ? data.aws_ami.rhel9.id : (
+            var.comet_ec2_ami_type == "ubuntu18" ? data.aws_ami.ubuntu18.id : (
+              var.comet_ec2_ami_type == "ubuntu20" ? data.aws_ami.ubuntu20.id : (
+                var.comet_ec2_ami_type == "ubuntu22" ? data.aws_ami.ubuntu22.id : (
+                  null))))))))
   instance_type          = var.comet_ec2_instance_type
   key_name               = var.comet_ec2_key
   count                  = var.comet_ec2_instance_count
