0d0e5403749ddf30167c3e9cda9ab71eb65a9034
christopher-comet <christopherw@comet.com>
2025-05-16 12:18:58 -0400

    add public rt

--- modules/comet_vpc/main.tf
+++ modules/comet_vpc/main.tf
@@ -45,6 +45,6 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id = module.vpc.vpc_id
   service_name = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = module.vpc.private_route_table_ids
+  route_table_ids   = concat(module.vpc.private_route_table_ids, module.vpc.public_route_table_ids)
   tags = local.tags
 }
