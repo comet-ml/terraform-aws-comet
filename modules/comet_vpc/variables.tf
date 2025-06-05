ace4ddc02b58075d72b5a5c0ffe953ca07ca428f
christopher-comet <christopherw@comet.com>
2025-06-05 14:06:02 -0400

    make module use var and update k8s

--- modules/comet_vpc/variables.tf
+++ modules/comet_vpc/variables.tf
@@ -23,3 +23,9 @@ variable "region" {
   description = "AWS region to provision resources in"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
