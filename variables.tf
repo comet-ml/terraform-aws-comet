39791c1a1723eba394397472526507347341bd8c
Jonathan Stanley <15162227+jms200@users.noreply.github.com>
2026-03-05 11:35:44 -0500

    fix: use rds_enhanced_monitoring_interval to align with stsaas fork naming

--- variables.tf
+++ variables.tf
@@ -645,7 +645,7 @@ variable "environment_tag" {
 }
 
 #### RDS Monitoring ####
-variable "rds_monitoring_interval" {
+variable "rds_enhanced_monitoring_interval" {
   description = "Enhanced Monitoring interval in seconds. Valid values: 0, 1, 5, 10, 15, 30, 60. Set to 0 to disable."
   type        = number
   default     = 0
